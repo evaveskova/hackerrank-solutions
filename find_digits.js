67271f010abf36156790f74b676370b7923c127c
Eva <evaveskova@gmail.com>
2019-08-13 19:40:35 +0200

    Added the FindDigits solution

--- find_digits.js
+++ find_digits.js
@@ -1,1 +1,58 @@
+'use strict';
 
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Start of challenge solution
+function findDigits(n) {
+    let digitsDivisible = 0;
+    let testing = n.toString().split("");
+    let digit = 0;
+
+    testing.forEach(function (digit) {
+    if (n % parseInt(digit) === 0) {
+        digitsDivisible++;
+
+}
+    })
+    return digitsDivisible;
+}
+
+// End of challenge solution
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine(), 10);
+
+        let result = findDigits(n);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
