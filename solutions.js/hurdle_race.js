7d6f38f351d3bf66a9a3e1a1a670d8cdd8152e8c
Eva <evaveskova@gmail.com>
2019-08-13 20:01:40 +0200

    Added the Hurdle Race solution

--- solutions.js/hurdle_race.js
+++ solutions.js/hurdle_race.js
@@ -1,1 +1,58 @@
+'use strict';
 
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Start of solution
+function hurdleRace(k, height) {
+    var potionsNeeded = 0;
+
+    height.forEach(function (hurdle) {
+        if(hurdle > k){
+          potionsNeeded += hurdle - k;
+          k += hurdle - k;
+        }
+        console.log(potionsNeeded);
+    });
+return potionsNeeded;
+}
+// End of solution
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nk = readLine().split(' ');
+
+    const n = parseInt(nk[0], 10);
+
+    const k = parseInt(nk[1], 10);
+
+    const height = readLine().split(' ').map(heightTemp => parseInt(heightTemp, 10));
+
+    let result = hurdleRace(k, height);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
